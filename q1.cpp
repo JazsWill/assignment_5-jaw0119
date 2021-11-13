c1d459a2e65882e8a147b5b7fb79420fef944514
jaw0119 <92323456+JazsWill@users.noreply.github.com>
2021-11-13 17:05:09 -0600

    Update q1.cpp

--- q1.cpp
+++ q1.cpp
@@ -1,1 +1,44 @@
 #include <iostream>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+// Assignment 5 - Exercise 1
+
+void grammarly(char sentence[])
+{  
+    // Capitalize the first letter of the sentence
+    if (sentence[0] != ' ')
+    {
+        sentence[0] = toupper(sentence[0]);
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        // Ignore extra spaces
+        if ((sentence[i] == ' ') && (sentence[i + 1] == ' '))
+        {
+            continue;
+        }
+        
+        // Force lowercase letters for the remaining characters
+        if (i > 0)
+        {
+            sentence[i] = tolower(sentence[i]);
+        }
+    }
+
+    cout << sentence;
+}
+
+int main()
+{
+    // Assignment 5 - Exercise 1
+
+    char sentence[100];
+    cout << "Input a sentence (No more than 100 characters): ";
+    cin.getline(sentence, 100);
+    grammarly(sentence);
+
+    return 0;
+}
